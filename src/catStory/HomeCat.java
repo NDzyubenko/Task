3029b9189a29153f1b72fbe8c9fa96a4c79ff8db
qa dev-3 <qa1@dev-3.com>
2021-05-14 13:09:00 +0300

    Fixed abstract class

---
+++ src/catStory/HomeCat.java
@@ -0,0 +1,26 @@
+package catStory;
+public class HomeCat extends Cat {
+    private Master master;
+
+    public HomeCat(String Name, String Breed, Master master) {
+        this.Name = Name;
+        this.Breed = Breed;
+        this.master = master;
+    }
+
+    public String getName(){
+        return Name;
+    }
+
+    public String getBreed(){
+        return Breed;
+    }
+
+    public void layOnSofa() {
+        System.out.println(Name + ": Meow!\n");
+    }
+
+    public void spoilShoes() {
+        master.decreaseMoney();
+    }
+}
